67bbbd4fb15b076a1a2fbec01505b0d629bc9794
Mundurujoana <mundujoana@gmail.com>
2022-06-27 13:17:51 +0300

    JavaScript Comparision and Logical Operators

---
+++ ComparisonandLogicalOperators.js
@@ -0,0 +1,72 @@
+const a=5, b=5, c='john'
+console.log(a ==5)
+console.log(a ==5)
+console.log(a =='John')
+
+
+// Strict Equal to Operator
+const we = 2;
+console.log(we === 2); // true
+console.log(we === '2'); // false
+
+
+// Strict Not Equal to Operator
+const se = 2, ze = 'hello';
+console.log(se !== 2); // false
+console.log(se !== '2'); // true
+console.log(ze !== 'Hello'); // true
+
+
+// Greater than Operator
+const re = 3;
+console.log(re > 2); // true
+
+
+// Less than Operator
+const as = 3, bs = 2;
+console.log(as < 2); // false
+console.log(bs < 3); // true
+
+
+// Greater than or Equal to Operator
+const an = 3;
+console.log(an >= 3); //true
+
+
+// Not Equal to Operator
+const de = 3, be = 'hello';
+
+// not equal operator
+console.log(de != 2); // true
+console.log(be != 'Hello'); // true
+
+
+// Less than or Equal to Operator
+const ab = 2;
+
+// less than or equal operator
+console.log(ab <= 3) // true
+console.log(ab <= 2); // true
+
+//Logical AND Operator
+const r = true, d = false;
+const s = 4;
+
+// logical AND
+console.log(r && d); // true
+console.log(r && d);  // false
+
+console.log((s > 2) && (s < 2)); // false
+
+// Logical OR Operator
+const m = true, n = false, z = 4;
+
+console.log(m || n); // true
+console.log(m || b); // false
+console.log((z>2) || (z<2)); // true
+
+// Logical NOT Operator
+const x =true
+const y=false
+console.log(!x)
+console.log(!y)
